57a6739e13c59e06e1d0e0680ed3855125f3de23
phonggiotai <114666846+phonggiotai@users.noreply.github.com>
2024-11-19 00:08:38 +0700

    use migrate instead of alembic

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -4,9 +4,11 @@ from config import Config
 from flask_mail import Mail
 from .models.base import db
 from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
 
 mail = Mail()
 celery = Celery(__name__)  # Initialize without broker
+migrate = Migrate()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -15,7 +17,8 @@ def create_app(config_class=Config):
     
     db.init_app(app)
     mail.init_app(app)
-    
+    migrate.init_app(app, db)
+    from app.models import user, group, login_attempt, category, unit, food, shopping, token, role, meal_plan, recipe
     # Configure celery with the broker URL from config
     celery.conf.update(
         broker_url=app.config['CELERY_BROKER_URL'],
@@ -35,7 +38,5 @@ def create_app(config_class=Config):
     from .controllers.user import user_api
     app.register_blueprint(user_api, url_prefix="/user")
 
-    with app.app_context():
-        db.create_all()
 
     return app
