12afe972e58c6c1449d0149d1c423cb58f66d68c
phonggiotai <114666846+phonggiotai@users.noreply.github.com>
2024-12-08 01:50:35 +0700

    database: add "updated_at" and "status" to FridgeItem

--- server/app/models/fridge_item.py
+++ server/app/models/fridge_item.py
@@ -14,18 +14,23 @@ class FridgeItem(Base):
     quantity: Mapped[int] = mapped_column(Double, nullable=False)
     expiration_date: Mapped[datetime] = mapped_column(DateTime, nullable=False)
     created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
+    updated_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    status: Mapped[str] = mapped_column(String(10), default='active')
 
 
     food = relationship('Food', backref='fridge_items')
     user = relationship('User', backref='fridge_items')
 
 
-    def __init__(self, owner_id, food_id, user_id, quantity, expiration_date):
+    def __init__(self, owner_id, food_id, user_id, quantity, expiration_date, created_at=None, updated_at=None, status=None):
         self.owner_id = owner_id
         self.added_by = user_id
         self.food_id = food_id
         self.quantity = quantity
         self.expiration_date = expiration_date
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
+        self.status = 'active'
 
 
     def as_dict(self):
